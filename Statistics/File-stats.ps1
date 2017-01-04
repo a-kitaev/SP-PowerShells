85b84a1d1ef3bb6f1f3432cc43933ba70023183d
a-kitaev <a.kitaev@outlook.com>
2017-01-04 14:07:22 +0300

    Update File-stats.ps1
    Rewrote using NoteProperty and Export-CSV

--- Statistics/File-stats.ps1
+++ Statistics/File-stats.ps1
@@ -1,27 +1,27 @@
-$sitecol = Get-SPSite -Identity "https://your.site.com"
-
-foreach($site in $sitecol)
-{
-  foreach($s in $site.AllWebs)
-  {
-    foreach($l in $s.lists)
-    {
-      if($l.BaseType -eq "DocumentLibrary") {
+$sitecol = Get-SPSite -Identity "https://your.site.com/"
+$Items = @() 
+foreach($site in $sitecol){
+  foreach($web in $site.AllWebs){
+  Write-Host "Checking " $Web.Title " site" -foregroundcolor Green
+    foreach($list in $web.lists){
+      if($list.BaseType -eq "DocumentLibrary") {
+	Write-Host $list.title "contains " $list.Items.count " items"
         $itemSize = 0
-        foreach($item in $l.items) {
-          $itemSize = ($item.file).length
-		  $filesize = [string][Math]::Round(($itemSize/1KB),2)
-		  $filename = ($item.file).name
-		  $filename2 = ($item.file).parentfolder
-		  $filename3 = $item["Editor"]
-		  $filename4 = $item["Author"]
-		  $filename5 = $item["Modified"]
-		  $filename6 = $item["Created"]
-		  $title = $filesize + "," + $s.Title + "," + $l.Title + "," + $filename + "," + $filename2 + "," + $filename3 + "," + $filename4 + "," + $filename5 + "," + $filename6
-		  $title >> "output.csv"
+        foreach($item in $list.items) {
+		$record = New-Object -TypeName System.Object
+         	$itemSize = ($item.file).length
+		$record | Add-Member NoteProperty FileSize ([string][Math]::Round(($itemSize/1KB),2))
+		$record | Add-Member NoteProperty FileName (($item.file).name)
+		$record | Add-Member NoteProperty FilePath (($item.file).parentfolder)
+		$record | Add-Member NoteProperty Editor ($item["Editor"])
+		$record | Add-Member NoteProperty Author ($item["Author"])
+		$record | Add-Member NoteProperty DateModified ($item["Modified"])
+		$record | Add-Member NoteProperty DateCreated ($item["Created"])
+		$Items += $record
         }
       } 
     }
-    $s.Dispose()
+    $web.Dispose()
   }
 }
+$Items | Export-CSV "c:\scripts\stats.csv" -Encoding UTF8
